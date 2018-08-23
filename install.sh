caaa3f29183a2776aa538fc8f18a57299d0632d1
William Tobin <tobinw2@rpi.edu>
2018-08-23 14:44:22 -0400

    adding vim,emacs,git,tmux to install.sh and module.yaml and using only the system install of gcc to build cmake

--- install.sh
+++ install.sh
@@ -1,11 +1,17 @@
 #!/bin/bash 
 
+spack install vim@8.1.0001%gcc@rhel7_4.8.5
+spack install emacs@26.1%gcc@rhel7_4.8.5
+spack install git@2.18.0%gcc@rhel7_4.8.5
+spack install tmux@2.7%gcc@rhel7_4.8.5
+
+spack install cmake%gcc@rhel7_4.8.5
+
 spack install gcc@4.8.5%gcc@rhel7_4.8.5
 spack install gcc@7.3.0%gcc@rhel7_4.8.5
 
 for compiler in gcc@4.8.5 gcc@7.3.0; do 
   echo $compiler
-  spack install cmake%${compiler}
   spack install mpich@3.2.1%${compiler}
   spack install parmetis%${compiler}
   spack install zoltan%${compiler} +parmetis
