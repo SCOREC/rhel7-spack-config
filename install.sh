0ffaf406a38bc7eee9c88a846d8a7fe49a7ab73c
William Tobin <tobinw2@rpi.edu>
2018-09-11 11:28:47 -0400

    build clang and then make the compiler non-executable to avoid developers using an unsupported compiler (should update so the directory isn't hard-coded), run tests on all the petsc installs

--- install.sh
+++ install.sh
@@ -1,16 +1,20 @@
-#!/bin/bash 
+#!/bin/bash
 
-spack install vim@8.1.0001%gcc@rhel7_4.8.5
+spack install vim@8.1.0001%gcc@rhel7_4.8.5 +python
 spack install emacs@26.1%gcc@rhel7_4.8.5
 spack install git@2.18.0%gcc@rhel7_4.8.5
 spack install tmux@2.7%gcc@rhel7_4.8.5
 
 spack install cmake%gcc@rhel7_4.8.5
 
+spack install llvm%gcc@rhel7_4.8.5 +clang -lldb
+chmod -x /opt/scorec/spack/install/linux-rhel7-x86_64/gcc-rhel7_4.8.5/llvm-6.0.1-2haksygco2dmqxd2ucqb2b53derwcuko/bin/clang
+chmod -x /opt/scorec/spack/install/linux-rhel7-x86_64/gcc-rhel7_4.8.5/llvm-6.0.1-2haksygco2dmqxd2ucqb2b53derwcuko/bin/clang-6.0
+
 spack install gcc@4.8.5%gcc@rhel7_4.8.5
 spack install gcc@7.3.0%gcc@rhel7_4.8.5
 
-for compiler in gcc@4.8.5 gcc@7.3.0; do 
+for compiler in gcc@4.8.5 gcc@7.3.0; do
   echo $compiler
   spack install mpich@3.2.1%${compiler}
   spack install parmetis%${compiler}
@@ -21,18 +25,18 @@ done
 
 compiler=gcc@7.3.0
 #uint/real-c hdf5 with fortran
-spack install petsc%${compiler} ^hdf5 +fortran
+spack install --keep-stage --test=ALL petsc%${compiler} ^hdf5 +fortran
 #uint/real-c
-spack install petsc%${compiler}  
+spack install --keep-stage --test=ALL petsc%${compiler}
 #ulong/real-c
-spack install petsc%${compiler} +int64
+spack install --keep-stage --test=ALL petsc%${compiler} +int64
 
 #uint/complex-c
-spack install petsc%${compiler} +complex
+spack install --keep-stage --test=ALL petsc%${compiler} +complex +mumps
 #ulong/complex-c
-spack install petsc%${compiler} +complex +int64
+spack install --keep-stage --test=ALL petsc%${compiler} +complex +int64 +mumps
 
 #uint/complex-cxx
-spack install petsc%${compiler} clanguage=C++ +complex
+spack install --keep-stage --test=ALL petsc%${compiler} clanguage=C++ +complex +mumps
 #ulong/complex-cxx
-spack install petsc%${compiler} clanguage=C++ +complex +int64
+spack install --keep-stage --test=ALL petsc%${compiler} clanguage=C++ +complex +int64 +mumps
