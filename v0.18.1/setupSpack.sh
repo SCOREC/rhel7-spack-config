f8c1cc2e588b402fcf910f9b8e57df86ad524fde
Cameron Smith <smithc11@rpi.edu>
2023-09-18 16:22:28 -0400

    create new clean v0.18.1 env
    prior version had spack develop 0.19.1 (?) merged into it

--- v0.18.1/setupSpack.sh
+++ v0.18.1/setupSpack.sh
@@ -1,7 +1,7 @@
 export SPACK_ROOT=/opt/scorec/spack/spack0.18.1
 export PATH=$SPACK_ROOT/bin:$PATH
 source $SPACK_ROOT/share/spack/setup-env.sh
-envDir=$SPACK_ROOT/v0181_0
+envDir=$SPACK_ROOT/v0181_2
 export SPACK_DISABLE_LOCAL_CONFIG=true #disable use of user and system config files
 export SPACK_USER_CACHE_PATH=$envDir
 spack env list
