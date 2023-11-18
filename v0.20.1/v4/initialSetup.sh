352eb27e8becdcaca9abaaa4b6f3206fd9cec436
Cameron Smith <smithc11@rpi.edu>
2023-11-17 22:10:49 -0500

    new env with correct concretizer settings...

---
+++ v0.20.1/v4/initialSetup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+spack add gcc@7.4.0 +binutils %gcc@4.8.5_rhel7 # Step 1
+spack concretize
+spack install --fail-fast
+spack compiler add $(spack location -i gcc@7.4.0)
+
+spack add gcc@11.2.0 %gcc@7.4.0
+spack concretize
+spack install --fail-fast
+spack compiler add $(spack location -i gcc@11.2.0)
+
+spack add mpich %gcc@11.2.0
+spack concretize
+spack install --fail-fast
+
+spack add openmpi +cuda %gcc@11.2.0
+spack concretize
+spack install --fail-fast
